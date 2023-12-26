5ad6bc0eda38a9111ca76957c949d8ae928aa396
PiotrStachowicz <337942@uwr.edu.pl>
2023-12-27 00:20:10 +0100

    Changed to https

--- server/start_server.js
+++ server/start_server.js
@@ -1,7 +1,18 @@
 let server = require('C:/Users/pansm/Cookie-Store/COOKIE-STORE/app.js')
-let http = require('http')
+let https = require('https')
+var fs = require('fs')
 let port = 5000
 
-http.createServer(server).listen(port)
+async function start_server(){
+    var pfx = await fs.promises.readFile('./server/zadanie2.pfx')
+    https.createServer(
+        {
+            pfx: pfx,
+            passphrase: 'zadanie2'
+        },
+        server
+    ).listen(port);
+}
 
+start_server()
 console.log(`server live on http://localhost:${port}`)
